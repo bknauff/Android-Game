a9d18075a6ca2952984bbe49ce6428f9136aef9e
bknauffgit config --global user.name bknauff <bkjr88@verizon.net>
2017-07-22 17:02:36 -0400

    More Game Functionality
    Added more functionality to the game and fixed some bugs. Game is in an
    OK state as of now. Need to fix a bug when starting the game where the
    camera decides to move and cant see the objects/game.

--- Android Game/Assets/Scripts/Stacking.cs
+++ Android Game/Assets/Scripts/Stacking.cs
@@ -10,6 +10,15 @@ public class Stacking : MonoBehaviour {
     private int scoreCount = 0;
     private float itemTransition = 0.0f;
     private float itemSpeed = 2.5f;
+    private bool isMovingX = true;
+    private float secPosition;
+    private Vector3 Iposition;
+    private const float SPEED = 5.0f;
+    private Vector3 lastPosition;
+    private const float ERR_MARGIN = 0.1f;
+    private int combo;
+    private Vector2 bounds = new Vector2(BOUND_SIZE, BOUND_SIZE);
+    private bool gameOver = false;
 
 	private void Start () {
         Stack = new GameObject[transform.childCount];
@@ -34,31 +43,89 @@ public class Stacking : MonoBehaviour {
             }
         }
         MoveItem();
+
+        //Moving the stack
+        transform.position = Vector3.Lerp(transform.position, Iposition, SPEED * Time.deltaTime);
     }
 
     private void MoveItem()
     {
+        if (gameOver)
+        {
+            return;
+        }
         itemTransition += Time.deltaTime * itemSpeed;
-        Stack[stackIndex].transform.localPosition = new Vector3(Mathf.Sin(itemTransition * BOUND_SIZE), scoreCount, 0);
+        if (isMovingX)
+        {
+            Stack[stackIndex].transform.localPosition = new Vector3(Mathf.Sin(itemTransition) * BOUND_SIZE, scoreCount, secPosition);
+        }
+        else
+        {
+            Stack[stackIndex].transform.localPosition = new Vector3(secPosition, scoreCount, Mathf.Sin(itemTransition) * BOUND_SIZE);
+        }
     }
 
     private void SpawnItem()
     {
+        lastPosition = Stack[stackIndex].transform.localPosition;
         stackIndex--;
         if(stackIndex < 0)
         {
             stackIndex = transform.childCount - 1;
         }
+        Iposition = (Vector3.down) * scoreCount;
         Stack[stackIndex].transform.localPosition = new Vector3(0, scoreCount, 0);
     }
 
     private bool PlaceItem()
     {
+        Transform t = Stack[stackIndex].transform;
+
+        if (isMovingX)
+        {
+            float deltaX = lastPosition.x - t.position.x;
+            if(Mathf.Abs(deltaX) > ERR_MARGIN)
+            {
+                //Get rid of part of item that is not in line with item below it
+                combo = 0;
+                bounds.x -= Mathf.Abs(deltaX);
+                if(bounds.x <= 0)
+                {
+                    return false;
+                }
+                float middle = lastPosition.x + t.localPosition.x / 2;
+                t.localScale = new Vector3(bounds.x, 1, bounds.y);
+                t.localPosition = new Vector3(middle - (lastPosition.x / 2), scoreCount, lastPosition.z);
+            }
+        }
+        else
+        {
+            float deltaZ = lastPosition.z - t.position.z;
+            if (Mathf.Abs(deltaZ) > ERR_MARGIN)
+            {
+                //Get rid of part of item that is not in line with item below it
+                combo = 0;
+                bounds.y -= Mathf.Abs(deltaZ);
+                if (bounds.y <= 0)
+                {
+                    return false;
+                }
+                float middle = lastPosition.z + t.localPosition.z / 2;
+                t.localScale = new Vector3(bounds.x, 1, bounds.y);
+                t.localPosition = new Vector3(lastPosition.x, scoreCount, middle - (lastPosition.z / 2));
+            }
+        }
+        secPosition = (isMovingX)
+            ? t.localPosition.x
+            : t.localPosition.z;
+        isMovingX = !isMovingX;
         return true;
     }
 
     private void EndGame()
     {
-
+        Debug.Log("Lose");
+        gameOver = true;
+        Stack[stackIndex].AddComponent<Rigidbody>();
     }
 }
